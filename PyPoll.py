7b4221693c7c857e3e44f8ea0b6a04440c203eea
Eric Langendorff <eric.langendorff@asiafoundation.org>
2022-04-20 03:19:27 -0700

    Adding election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,16 @@
-from email import header
+# Import dependencies
 import os
 import csv
 
-# The data we need to retrieve
+# Retrieve data
 
 # Assign a name for the path to the file to load
 file_to_load = os.path.join('resources', 'election_results.csv')
+
+# Initialize vote counter, candidates list, and vote-count dictionary
+total_votes = 0
+candidate_options = []
+candidate_votes = {'winner':{'name':'nobody','votes':0}}  # Include a 'winner' key whose value will hold the name and votes of the eventual winner
 
 # Open the election results, and read the file
 # # election_data = open(file_to_load, 'r')
@@ -14,22 +19,34 @@ with open(file_to_load) as election_data:
     # Read the file object
     file_reader = csv.reader(election_data)
     
-    # Print the header row
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
     
-    # for row in file_reader:
-    #     print(row)
+    # Loop through each row in the CSV file
+    for row in file_reader:
+    
+        # 1. Count the total number of votes cast
+        total_votes += 1
 
-# 1. The total number of votes cast
+        # 2. Create list of candidates who received votes
+        if row[2] not in candidate_options:     # If the candidate (candidates' names are at index 2) on the current row is new to the list, then
+            candidate_options.append(row[2])    # add the candidate's name to the list,
+            candidate_votes[row[2]] = 0         # and create a vote counter for the candidate
+        
+        # 3. Count the votes for the current row's candidate
+        candidate_votes[row[2]] += 1
 
-# 2. Complete list of candidates who received votes
-
-# 3. Total number of votes won by each candidate  # NOTE: #3 and #4 were reversed in the Module, but since percentage is dependant on total…
-
-# 4. Percentage of votes won by each candidate
-
-# 5. Winner of election based on popular vote
+# Print the results
+print(f'\nTotal Votes Cast: {total_votes:,}') # total number of votes
+candidate_options.sort()                    # Sort the candidate list
+print('\nCandidates:')
+for candidate in candidate_options:         # List each candidate, along with candidate's vote count and vote proportion (%)
+    print(f'{candidate}: {candidate_votes[candidate]:,} votes, {100 * candidate_votes[candidate] / total_votes:.1f}% of total')
+    if candidate_votes['winner']['votes'] < candidate_votes[candidate]: # If the candidate has more votes than the current 'winner', then
+        candidate_votes['winner']['name'] = candidate                   # set the potential winner's name to the current candidate's,
+        candidate_votes['winner']['votes'] = candidate_votes[candidate] # and record the candidate's vote count
+# print(f'\nWinner: {candidate_votes['winner']['name']}!')    # 5. The winner!
+print(f"\nWinner: {candidate_votes['winner']['name']}!\n")
 
 # Create a filename name to a direct or indirect path to the file.
 file_to_save = os.path.join('analysis', 'election_analysis.txt')
